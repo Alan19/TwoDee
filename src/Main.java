646896dca932a5c6bdb3809bd0666ab59fe07747
Alan19 <Alan19@users.noreply.github.com>
2018-08-27 00:25:56 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Copy args and add 1 before dice rolls without a number
+        ArrayList<String> diceList = new ArrayList<String>();
+        if (args.length == 0){
+            System.out.println("Enter your dice!");
+            Scanner input = new Scanner(System.in);
+            diceList.addAll(Arrays.asList(input.nextLine().split(" ")));
+        }
+        else {
+            diceList.addAll(Arrays.asList(args));
+        }
+        //Check for dice that does not have a number before d and add 1 to represent one die
+        for (int i = 0; i < diceList.size(); i++) {
+            String die = diceList.get(i);
+            if (!Character.isDigit(die.charAt(0))) {
+                diceList.set(i, "1" + die);
+            }
+        }
+        //Generate first line of function
+        String line1 = "function: highest N:n of " + generateParameters(diceList) + "{";
+        String line2 = "result: {1..N}@[sort {" + generateListLengthAsLetters(diceList.size()) + "}]\n}";
+        String line3 = "output [highest 2 of " + generateDiceListAsString(diceList) + "]";
+        System.out.print(line1 + "\n" + line2 + "\n" + line3);
+    }
+
+    //Generates the dice being rolled, separated by a space (1d6, 2d10, 3d8)
+    private static String generateDiceListAsString(ArrayList<String> diceList) {
+        return String.join(" ", diceList);
+    }
+
+    /*
+    Generate the number of types of dice being rolled (the length of the ArrayList diceList) as a string. For example,
+    it will return "A, B, C" if there are 3 types of dice being rolled (1d12, 2d10, 3d6)
+     */
+    private static String generateListLengthAsLetters(int size) {
+        StringBuilder outputLetters = new StringBuilder();
+        for (int i = 0; i < size; i++){
+            outputLetters.append((char) (65 + i));
+            if (!(i + 1 == size)){
+                outputLetters.append(", ");
+            }
+        }
+        return outputLetters.toString();
+    }
+
+    //Generate the parameter list based on number of dice being rolled. For example, if there are 3 dice being rolled,
+    //it will return "A:s B:s C:s"
+    private static String generateParameters(ArrayList<String> dice) {
+        StringBuilder parameters = new StringBuilder();
+        for (int i = 0; i < dice.size(); i++){
+            parameters.append((char) (65 + i)).append(":s ");
+        }
+        return parameters.toString();
+    }
+
+}
