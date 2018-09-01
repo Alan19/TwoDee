50c5978c1e32869eafa7661f2f04aa3d3675439d
Alan19 <Alan19@users.noreply.github.com>
2018-09-01 00:15:19 -0400

    Add properties support

--- src/discord/TwoDee.java
+++ src/discord/TwoDee.java
@@ -4,26 +4,36 @@ import logic.StatisticsGenerator;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.util.logging.ExceptionLogger;
 
+import java.io.*;
+import java.util.Properties;
+
 import static java.lang.System.*;
 
 public class TwoDee {
-    public static void main(String[] args) {
-        // Insert your bot's token here
-        String token = "NDg0NTE1MDMzMjIxNDMxMzAw.DmjPTQ.9CGWp73_sFXNOjoB63TYCAd0b2M";
-        new DiscordApiBuilder().setToken(token).login().thenAccept(api -> {
-            // Add a listener that outputs statistics when you prefix a message with ~s
-            api.addMessageCreateListener(event -> {
-                if (event.getMessage().getContent().startsWith("~s")) {
-                    StatisticsGenerator statistics = new StatisticsGenerator(event.getMessage().getContent());
-                    event.getChannel().sendMessage(statistics.generateStatistics());
-                }
-            });
 
-            // Print the invite url of your bot
-            out.println("You can invite the bot by using the following url: " + api.createBotInvite());
-        })
-                // Log any exceptions that happened
-                .exceptionally(ExceptionLogger.get());
+    public static void main(String[] args) {
+        try {
+            Properties prop = new Properties();
+            prop.load(new FileInputStream("src/bot.properties"));
+            String token = prop.getProperty("token");
+            new DiscordApiBuilder().setToken(token).login().thenAccept(api -> {
+                // Add a listener that outputs statistics when you prefix a message with ~s
+                api.addMessageCreateListener(event -> {
+                    if (event.getMessage().getContent().startsWith("~s")) {
+                        StatisticsGenerator statistics = new StatisticsGenerator(event.getMessage().getContent());
+                        event.getChannel().sendMessage(statistics.generateStatistics());
+                    }
+                });
+
+                // Print the invite url of your bot
+                out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+            })
+                    // Log any exceptions that happened
+                    .exceptionally(ExceptionLogger.get());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
