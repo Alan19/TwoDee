de28a753141297e59081bb7f7d21b707cc7c5808
Alan19 <Alan19@users.noreply.github.com>
2019-11-28 17:06:44 -0500

    Add cancel react for emoji removal and have embed only show up for servewide removal

--- src/main/java/commands/EmojiPurgeCommand.java
+++ src/main/java/commands/EmojiPurgeCommand.java
@@ -1,11 +1,11 @@
 package commands;
 
-import com.vdurmont.emoji.EmojiParser;
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import discord.TwoDee;
 import logic.PlotPointEnhancementHelper;
 import logic.RandomColor;
+import org.apache.commons.math3.util.Pair;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.Message;
@@ -23,39 +23,43 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class EmojiPurgeCommand implements CommandExecutor {
-    private int emojis = 0;
-    private int messagesToClear = 0;
-
     @Command(async = true, aliases = {"~emojipurge", "~killeverylastoneofthem", "~scorchedearth", "~removeemojis", "~p"}, description = "Removes all plot point enhancement emojis from this channel", usage = "~removeemojis [all]")
     public void purgeEmojis(String[] params, DiscordApi api, MessageAuthor author, Message message, TextChannel channel, Server server) {
         boolean removeFromAllChannels = params.length == 1 && params[0].equals("all") || message.getContent().equals("~killeverylastoneofthem") || message.getContent().equals("~scorchedearth");
+        AtomicInteger totalMessages = new AtomicInteger();
+        AtomicInteger totalEmojis = new AtomicInteger();
         if (removeFromAllChannels) {
-            server.getTextChannels().forEach(this::removeAllPlotPointEmojisFromChannel);
+            //Remove all of the emojis and then map the TextChannels to a stream
+            server.getTextChannels().stream().map(this::removeAllPlotPointEmojisFromChannel).forEach(emojiInfoPair -> {
+                totalMessages.addAndGet(emojiInfoPair.getKey());
+                totalEmojis.addAndGet(emojiInfoPair.getValue());
+                CompletableFuture<Message> sentEmbed = new MessageBuilder()
+                        .setEmbed(new EmbedBuilder()
+                                .setColor(RandomColor.getRandomColor())
+                                .setAuthor(author)
+                                .setTitle(TwoDee.getServerwideEmojiRemovalMessage())
+                                .addField("Messages with Emojis", String.valueOf(totalMessages))
+                                .addField("Emojis Removed", String.valueOf(totalEmojis))
+                                .setDescription("I removed all roll enhancement emojis in all channels."))
+                        .send(channel);
+                sentEmbed.thenAcceptAsync(StatisticsCommand::addCancelReactToMessage);
+
+            });
         } else {
             removeAllPlotPointEmojisFromChannel(channel);
         }
-        CompletableFuture<Message> sentEmbed = new MessageBuilder()
-                .setEmbed(new EmbedBuilder()
-                        .setColor(RandomColor.getRandomColor())
-                        .setAuthor(author)
-                        .setTitle(removeFromAllChannels ? TwoDee.getServerwideEmojiRemovalMessage() : TwoDee.getEmojiRemovalMessage())
-                        .addField("Messages with Emojis", String.valueOf(messagesToClear))
-                        .addField("Emojis Removed", String.valueOf(emojis))
-                        .setDescription("I removed all roll enhancement emojis in " + (removeFromAllChannels ? "all channels." : "this channel.")))
-                .send(channel);
-        sentEmbed.thenAcceptAsync(message1 -> message1.addReaction(EmojiParser.parseToUnicode(":x:")));
     }
 
     /**
      * Removes all enhancement emojis in a channel
      *
      * @param channel The channel to remove emojis from
+     * @return A pair with the left side as the number of messages and the right side as the number of emojis
      */
-    private void removeAllPlotPointEmojisFromChannel(TextChannel channel) {
+    private Pair<Integer, Integer> removeAllPlotPointEmojisFromChannel(TextChannel channel) {
         ArrayList<Message> enhancementEmojiMessageList = getMessagesWithEnhancementEmojis(channel);
-        if (enhancementEmojiMessageList != null && !enhancementEmojiMessageList.isEmpty()) {
-            messagesToClear = enhancementEmojiMessageList.size();
-            getNumberOfEmojisToRemove(enhancementEmojiMessageList);
+        int messagesToClear = enhancementEmojiMessageList.size();
+        int emojis = getNumberOfEmojisToRemove(enhancementEmojiMessageList);
             Message progressMessage = new MessageBuilder().setContent("Removing " + emojis + " emojis from " + messagesToClear + " messages!").send(channel).join();
             AtomicInteger current = new AtomicInteger();
             DecimalFormat df = new DecimalFormat("0.##");
@@ -65,7 +69,10 @@ public class EmojiPurgeCommand implements CommandExecutor {
                 System.out.println(current + "/" + messagesToClear + " (" + ((double) current.get() / messagesToClear * 100) + "%)");
                 progressMessage.edit("Removing " + emojis + " emojis from " + messagesToClear + " messages! " + current + "/" + messagesToClear + " (" + df.format((double) current.get() / messagesToClear * 100) + "%)");
             });
-        }
+        //Add delete emoji when done
+        progressMessage.edit(progressMessage.getContent() + "\nDone!");
+        StatisticsCommand.addCancelReactToMessage(progressMessage);
+        return new Pair<>(messagesToClear, emojis);
     }
 
     /**
@@ -73,15 +80,16 @@ public class EmojiPurgeCommand implements CommandExecutor {
      *
      * @param messageList The list of messages to search
      */
-    private void getNumberOfEmojisToRemove(ArrayList<Message> messageList) {
+    private int getNumberOfEmojisToRemove(ArrayList<Message> messageList) {
         PlotPointEnhancementHelper helper = new PlotPointEnhancementHelper();
-        emojis = 0;
+        AtomicInteger emojisInList = new AtomicInteger();
         messageList.stream()
                 .map(Message::getReactions)
                 .forEach(reactions -> reactions.stream()
                         .filter(Reaction::containsYou)
                         .filter(reaction -> helper.isEmojiEnhancementEmoji(reaction.getEmoji()))
-                        .forEach(reaction -> emojis += reaction.getCount()));
+                        .forEach(reaction -> emojisInList.addAndGet(reaction.getCount())));
+        return emojisInList.get();
     }
 
     /**
