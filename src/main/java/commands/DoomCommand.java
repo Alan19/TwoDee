8206020e4f237d5ca092fca6d068bb168d449985
Alan19 <Alan19@users.noreply.github.com>
2020-05-11 22:53:02 -0400

    Clean up doom command

--- src/main/java/commands/DoomCommand.java
+++ src/main/java/commands/DoomCommand.java
@@ -5,53 +5,54 @@ import de.btobastian.sdcf4j.CommandExecutor;
 import doom.DoomWriter;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.Message;
-import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 
 public class DoomCommand implements CommandExecutor {
-    @Command(aliases = {"~d", "~doom"}, description = "A command that allows you to manage doom points.", privateMessages = false, usage = "~t add|sub|set number_of_points")
-    //Generates an embed of the new doom value
+    @Command(aliases = {"~d", "~doom"}, description = "A command that allows you to manage doom points.", privateMessages = false, usage = "~d [add [number]|sub [number]|set number]")
     public void newDoom(Message message, TextChannel channel) {
         String messageContent = message.getContent();
-        String[] args = messageContent.split(" ");
-        DoomWriter doomWriter = new DoomWriter();
-        if (args.length == 1) {
-            new MessageBuilder()
-                    .setEmbed(new DoomWriter().generateDoomEmbed())
-                    .send(channel);
-        }
-        if (args.length != 3) {
-            new MessageBuilder()
-                    .setEmbed(new EmbedBuilder()
-                            .setTitle("Invalid command!"))
-                    .send(channel);
-        }
-        else {
-            String commandType = args[1];
-            int doomVal = Integer.parseInt(args[2]);
-
-            switch (commandType) {
-                case "add":
-                    new MessageBuilder()
-                            .setEmbed(doomWriter.addDoom(doomVal))
-                            .send(channel);
+        if (messageContent.matches("~(d|doom)( (add( [1-9]\\d*)?|sub( [1-9]\\d*)?|set [1-9]\\d*))?")) {
+            String[] args = messageContent.split(" ");
+            DoomWriter doomWriter = new DoomWriter();
+            switch (args.length) {
+                case 1:
+                    channel.sendMessage(new DoomWriter().generateDoomEmbed());
                     break;
-                case "sub":
-                    new MessageBuilder()
-                            .setEmbed(doomWriter.addDoom(doomVal * -1))
-                            .send(channel);
+                case 2:
+                    switch (args[1]) {
+                        case "add":
+                            channel.sendMessage(doomWriter.addDoom(1));
+                            break;
+                        case "sub":
+                            channel.sendMessage(doomWriter.addDoom(-1));
+                            break;
+                        default:
+                            throw new IllegalStateException("Unexpected value: " + args[1]);
+                    }
                     break;
-                case "set":
-                    new MessageBuilder()
-                            .setEmbed(doomWriter.setDoom(doomVal))
-                            .send(channel);
+                case 3:
+                    int doomVal = Integer.parseInt(args[2]);
+                    switch (args[1]) {
+                        case "add":
+                            channel.sendMessage(doomWriter.addDoom(doomVal));
+                            break;
+                        case "sub":
+                            channel.sendMessage(doomWriter.addDoom(doomVal * -1));
+                            break;
+                        case "set":
+                            channel.sendMessage(doomWriter.setDoom(doomVal));
+                            break;
+                        default:
+                            throw new IllegalStateException("Unexpected value: " + args[1]);
+                    }
                     break;
                 default:
-                    new MessageBuilder()
-                            .setEmbed(doomWriter.generateDoomEmbed())
-                            .send(channel);
+                    channel.sendMessage(new EmbedBuilder().setTitle("Invalid doom command!"));
                     break;
             }
+        }
+        else {
+            channel.sendMessage(new EmbedBuilder().setTitle("Invalid doom command!"));
         }
     }
 }
