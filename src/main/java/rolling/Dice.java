15ef708643b517a05257e383a893b2c810b133b4
Skylar <skylar.som@gmail.com>
2021-11-06 16:12:05 -0400

    Fix Pool Collector to properly show if error found

--- src/main/java/rolling/Dice.java
+++ src/main/java/rolling/Dice.java
@@ -1,5 +1,7 @@
 package rolling;
 
+import java.util.Objects;
+
 public class Dice {
     private final String name;
     private final int value;
@@ -15,5 +17,18 @@ public class Dice {
 
     public int getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dice dice = (Dice) o;
+        return value == dice.value && Objects.equals(name, dice.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, value);
     }
 }
--- src/test/java/rolling/PoolCollectorTest.java
+++ src/test/java/rolling/PoolCollectorTest.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
@@ -13,7 +14,7 @@ import java.util.stream.Stream;
 public class PoolCollectorTest {
 
     @Test
-    void validDiceShouldSuccess() {
+    void testValidDice() {
         Try<Pair<List<Dice>, List<Integer>>> dice = Stream.of(
                         Try.success(Either.<List<Dice>, Integer>left(Collections.singletonList(new Dice("d", 4))))
                 )
@@ -23,17 +24,33 @@ public class PoolCollectorTest {
         Assertions.assertEquals(dice.get(), Pair.<List<Dice>, List<Integer>>of(
                 Collections.singletonList(new Dice("d", 4)),
                 Collections.emptyList()
-        );
+        ));
     }
 
     @Test
-    void failedDiceShouldFailed() {
+    void testFailedDice() {
         Try<Pair<List<Dice>, List<Integer>>> dice = Stream.of(
-                        Try.<Either<List<Dice>, Integer>>failure(new IllegalArgumentException("Invalid Dice Type"))
+                        Try.<Either<List<Dice>, Integer>>failure(new IllegalArgumentException("Invalid Dice Type")),
+                        Try.success(Either.<List<Dice>, Integer>left(Collections.singletonList(new Dice("d", 4))))
                 )
                 .collect(PoolCollector.toTripleList());
 
         Assertions.assertTrue(dice.isFailure());
-        Assertions.assertEquals(dice.getCause(), new IllegalArgumentException("Invalid Dice Type"));
+        Assertions.assertEquals(dice.getCause().getMessage(), "Invalid Dice Type");
+    }
+
+    @Test
+    void testSuccessfulBonuses() {
+        Try<Pair<List<Dice>, List<Integer>>> bonus = Stream.of(
+                        Try.<Either<List<Dice>, Integer>>success(Either.right(4)),
+                        Try.<Either<List<Dice>, Integer>>success(Either.right(3))
+                )
+                .collect(PoolCollector.toTripleList());
+
+        Assertions.assertTrue(bonus.isSuccess());
+        List<Integer> resultEquals = new ArrayList<>();
+        resultEquals.add(4);
+        resultEquals.add(3);
+        Assertions.assertEquals(bonus.get().getRight(), resultEquals);
     }
 }
