2983dedf27a6bad5150da346cb60044b5d10081a
Skylar <skylar.som@gmail.com>
2022-03-31 23:14:35 -0400

    Clean up Autocomplete

--- src/main/java/listeners/LanguageAutocompleteListener.java
+++ src/main/java/listeners/LanguageAutocompleteListener.java
@@ -8,9 +8,18 @@ import org.javacord.api.interaction.SlashCommandInteractionOption;
 import org.javacord.api.interaction.SlashCommandOptionChoice;
 import org.javacord.api.listener.interaction.AutocompleteCreateListener;
 
+import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class LanguageAutocompleteListener implements AutocompleteCreateListener {
+    private static final List<String> PARAMETER_NAMES = ImmutableList.of(
+            LanguageCommand.LANGUAGE_NAME,
+            "language1",
+            "language2",
+            LanguageCommand.TARGET
+    );
+
     private final LanguageLogic languageLogic;
 
     public LanguageAutocompleteListener(LanguageLogic languageLogic) {
@@ -20,14 +29,22 @@ public class LanguageAutocompleteListener implements AutocompleteCreateListener 
     @Override
     public void onAutocompleteCreate(AutocompleteCreateEvent event) {
         final SlashCommandInteractionOption focusedOption = event.getAutocompleteInteraction().getFocusedOption();
-        if (ImmutableList.of(LanguageCommand.LANGUAGE_NAME, "language1", "language2", LanguageCommand.TARGET).contains(focusedOption.getName())) {
-            event.getAutocompleteInteraction().respondWithChoices(
-                    languageLogic.getLanguages()
-                            .stream()
-                            .filter(slashCommandOptionChoice -> slashCommandOptionChoice.toLowerCase().contains(focusedOption.getStringValue().orElse("").toLowerCase()))
-                            .limit(25)
-                            .map(s -> SlashCommandOptionChoice.create(s, s))
-                            .collect(Collectors.toList()));
+        if (PARAMETER_NAMES.contains(focusedOption.getName())) {
+            String optionValue = focusedOption.getStringValue()
+                    .map(string -> string.toLowerCase(Locale.ROOT))
+                    .orElse("");
+
+            event.getAutocompleteInteraction()
+                    .respondWithChoices(
+                            languageLogic.getLanguages()
+                                    .stream()
+                                    .filter(slashCommandOptionChoice -> slashCommandOptionChoice.toLowerCase()
+                                            .contains(optionValue)
+                                    )
+                                    .limit(25)
+                                    .map(s -> SlashCommandOptionChoice.create(s, s))
+                                    .collect(Collectors.toList())
+                    );
         }
     }
 
