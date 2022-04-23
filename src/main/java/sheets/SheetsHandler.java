01239d1adfdfee719ae9ed988639ef4ee651b6d1
Alan19 <Alan19@users.noreply.github.com>
2022-04-23 12:43:11 -0400

    Update dependencies

--- src/main/java/sheets/SheetsHandler.java
+++ src/main/java/sheets/SheetsHandler.java
@@ -8,7 +8,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
@@ -34,7 +34,7 @@ import java.util.stream.Collectors;
 
 public class SheetsHandler {
     private static final String APPLICATION_NAME = "Skill Lookup";
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
     private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
     private static final String CREDENTIALS_FILE_PATH = "resources/credentials.json";
