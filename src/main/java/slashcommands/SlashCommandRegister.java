693fa4c58fb4454a3de1342f5d89926eeaa9fd6f
Alan19 <Alan19@users.noreply.github.com>
2022-04-24 12:37:26 -0400

    Update to Java 17, remove test command

--- src/main/java/slashcommands/SlashCommandRegister.java
+++ src/main/java/slashcommands/SlashCommandRegister.java
@@ -22,7 +22,6 @@ public class SlashCommandRegister {
         CalculationLogic.setup(velen);
         AdvancementLogic.setupAdvancementCommand(velen);
         LanguageCommand.setup(velen, languageLogic);
-        NewRollLogic.setupRollCommand(velen);
 
         return velen;
     }
